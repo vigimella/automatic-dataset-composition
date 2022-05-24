c83624b260205b09015a1a303a75ded92da24fd6
vigimella <g.ciaramella98@gmail.com>
2022-05-24 23:24:01 +0200

    Now the tool is able to crate sub-folders

--- README.md
+++ README.md
@@ -8,8 +8,7 @@ This tool allows splitting out files to create a dataset.
 - Start this script and insert the parameters required.
 - Dataset is created splitting the elements in training (train,val), and test directories.
 
-## Suggestion: 
-This is a beta version. Save files in specific folder without using sub-folders. After the dataset creation, if you need 
-to create sub-folder, create them manually.
+## Update: 
+This is a beta version. Now the tool is able to crate sub-folders.
 
 ### Vigimella - 2022
--- main.py
+++ main.py
@@ -9,9 +9,58 @@ def percentage_calculation(perc_value, total_elm):
     return value_p
 
 
-def dataset_creation(original_dir_path, main_dir_name, dataset_split):
+def copy_and_split_files(dir_path, dataset_split, train_dir, val_dir, test_dir, main_dir, path_report):
 
     file_list_dir = list()
+    num_files = len([name for name in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, name))])
+
+    # calculating of elements to store in training dir
+    training_value = dataset_split.split('-')[0]
+    training_value = percentage_calculation(training_value, num_files)
+
+    # calculating of elements to store in test dir
+    test_value = dataset_split.split('-')[1]
+    test_value = percentage_calculation(test_value, num_files)
+
+    num_files_training = num_files - test_value
+
+    # calculating of elements to store in train dir
+    train_value = dataset_split.split('-')[0]
+    train_value = percentage_calculation(train_value, num_files_training)
+    # calculating of elements to store in val dir
+    val_value = dataset_split.split('-')[1]
+    val_value = percentage_calculation(val_value, num_files_training)
+
+    print(f'Directory contains {num_files} files. Dataset will be divided in:\n'
+          f' - Training Directory (train: {train_value} files, val: {val_value} files)\n - Test Directory {test_value} files \n',
+          file=open(path_report, 'a'))
+
+    for file in os.listdir(dir_path):
+        if file.endswith('.png'):
+            file = os.path.join(dir_path, file)
+            file_list_dir.append(file)
+
+    for file in file_list_dir[:train_value]:
+        shutil.copy(file, train_dir)
+        file_list_dir.remove(file)
+
+    for file in file_list_dir[:val_value]:
+        shutil.copy(file, val_dir)
+        file_list_dir.remove(file)
+
+    for file in file_list_dir[:test_value]:
+        shutil.copy(file, test_dir)
+        file_list_dir.remove(file)
+
+    if file_list_dir.__len__() > 0:
+        print(f'{file_list_dir.__len__()} file(s) cannot be included inside the dataset.\nAnyway your dataset has '
+              f'created at the following path {main_dir}, try another solution to improve performances. \n',
+              file=open(path_report, 'a'))
+
+
+def dataset_creation(original_dir_path, main_dir_name, dataset_split, path_report):
+    sub_folders = list()
+    new_sub_folders = list()
 
     # defining paths to directories creation and creation of them
 
@@ -42,49 +91,27 @@ def dataset_creation(original_dir_path, main_dir_name, dataset_split):
 
     else:
 
-        num_files = len(
-            [name for name in os.listdir(original_dir_path) if os.path.isfile(os.path.join(original_dir_path, name))])
+        for sub_folder in os.walk(original_dir_path):
+            path_folder = sub_folder[0].replace(APP_ROOT + '/', '')
+            if not path_folder == original_dir_path:
+                sub_folders.append(path_folder)
 
-        # calculating of elements to store in training dir
-        training_value = dataset_split.split('-')[0]
-        training_value = percentage_calculation(training_value, num_files)
+        if not sub_folders:
+            copy_and_split_files(original_dir_path, dataset_split, train_dir, val_dir, test_dir, main_dir_name, path_report)
+        else:
 
-        # calculating of elements to store in test dir
-        test_value = dataset_split.split('-')[1]
-        test_value = percentage_calculation(test_value, num_files)
-
-        num_files_training = num_files - test_value
-
-        # calculating of elements to store in train dir
-        train_value = dataset_split.split('-')[0]
-        train_value = percentage_calculation(train_value, num_files_training)
-        # calculating of elements to store in val dir
-        val_value = dataset_split.split('-')[1]
-        val_value = percentage_calculation(val_value, num_files_training)
-
-        print(f'Directory contains {num_files} files. Dataset will be divided in:\n'
-              f' - Training Directory (train: {train_value} files, val: {val_value} files)\n - Test Directory {test_value} files')
-
-        for file in os.listdir(original_dir_path):
-            if file.endswith('.png'):
-                file = os.path.join(original_dir_path, file)
-                file_list_dir.append(file)
-
-        for file in file_list_dir[:train_value]:
-            shutil.copy(file, train_dir)
-            file_list_dir.remove(file)
-
-        for file in file_list_dir[:val_value]:
-            shutil.copy(file, val_dir)
-            file_list_dir.remove(file)
-
-        for file in file_list_dir[:test_value]:
-            shutil.copy(file, test_dir)
-            file_list_dir.remove(file)
-
-        if file_list_dir.__len__() > 0:
-            print(f'{file_list_dir.__len__()} file(s) cannot be included inside the dataset.\nAnyway your dataset has '
-                  f'created at the following path {main_dir}, try another solution to improve performances.')
+            for path in sub_folders:
+                train_dir_sub = path.replace(original_dir_path, train_dir + '/')
+                val_dir_sub = path.replace(original_dir_path, val_dir + '/')
+                test_dir_sub = path.replace(original_dir_path, test_dir + '/')
+                new_sub_folders.append(train_dir_sub)
+                new_sub_folders.append(val_dir_sub)
+                new_sub_folders.append(test_dir_sub)
+                os.makedirs(train_dir_sub)
+                os.makedirs(val_dir_sub)
+                os.makedirs(test_dir_sub)
+                print(f'File: {path}', file=open(path_report, 'a'))
+                copy_and_split_files(path, dataset_split, train_dir_sub, val_dir_sub, test_dir_sub, main_dir_name, path_report)
 
 
 if __name__ == '__main__':
@@ -94,6 +121,7 @@ if __name__ == '__main__':
     input_dataset = input(f'Insert percentage (training-test): ')
     input_directory = input(f'Insert path of directory where files are stored: ')
     dataset_name = input(f'Insert the name of dataset: ')
+    report_name = input(f'Insert the name of report: ')
 
     check_one = input_dataset.split('-')[0]
     check_two = input_dataset.split('-')[1]
@@ -105,6 +133,8 @@ if __name__ == '__main__':
 
     else:
 
-        dataset_creation(input_directory, dataset_name, input_dataset)
-        #dataset_creation('/home/vigimella/Desktop/attempt_dataset/', 'prova', input_dataset)
-
+        path_report = os.path.join(APP_ROOT, report_name+'.txt')
+        if os.path.isfile(path_report):
+            os.remove(path_report)
+        print(f'Dataset division: {input_dataset}', file=open(path_report, 'a'))
+        dataset_creation(input_directory, dataset_name, input_dataset, path_report)
